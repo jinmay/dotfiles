1eb8aaf0b4958480f866e84d0940f8b2303aa3de
jinmay <britzegs@gmail.com>
2024-11-24 14:36:42 +0900

    Add .config/nvim/lua/plugins/bufferline.lua

---
+++ private_dot_config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,10 @@
+local bufferline = require("bufferline")
+
+bufferline.setup({
+	options = {
+		indicator = {
+			icon = "▎", -- this should be omitted if indicator style is not 'icon'
+			-- style = "underline",
+		},
+	},
+})
