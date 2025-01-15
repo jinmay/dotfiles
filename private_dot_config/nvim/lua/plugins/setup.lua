1106652007dfe1d2f418c72a45e7f62241b268fd
jinmay <britzegs@gmail.com>
2025-01-15 14:12:32 +0900

    Update .config/nvim/lua/plugins/setup.lua

--- private_dot_config/nvim/lua/plugins/setup.lua
+++ private_dot_config/nvim/lua/plugins/setup.lua
@@ -120,6 +120,11 @@ require("lazy").setup({
 
 	-- AI
 	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+	},
+	{
 		"yetone/avante.nvim",
 		event = "VeryLazy",
 		lazy = false,
@@ -154,7 +159,27 @@ require("lazy").setup({
 			},
 		},
 	},
-
+	{
+		"GeorgesAlkhouri/nvim-aider",
+		cmd = {
+			"AiderTerminalToggle",
+		},
+		keys = {
+			{ "<leader>a/", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
+			{ "<leader>as", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
+			{ "<leader>ac", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
+			{ "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
+			{ "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
+			{ "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
+		},
+		dependencies = {
+			"folke/snacks.nvim",
+			"nvim-telescope/telescope.nvim",
+			--- The below dependencies are optional
+			-- "catppuccin/nvim",
+		},
+		config = true,
+	},
 	{
 		"rachartier/tiny-inline-diagnostic.nvim",
 		event = "VeryLazy", -- Or `LspAttach`
