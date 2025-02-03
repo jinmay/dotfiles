f08f7451e273dbbac5cdb5991e17d6c014e2c42f
jinmay <britzegs@gmail.com>
2025-02-03 14:25:02 +0900

    Update .config/nvim/lua/plugins/fold.lua

--- private_dot_config/nvim/lua/plugins/fold.lua
+++ private_dot_config/nvim/lua/plugins/fold.lua
@@ -21,13 +21,14 @@ capabilities.textDocument.foldingRange = {
 	dynamicRegistration = false,
 	lineFoldingOnly = true,
 }
-local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-for _, ls in ipairs(language_servers) do
-	require("lspconfig")[ls].setup({
-		capabilities = capabilities,
-		-- you can add other fields for setting up lsp server in this table
-	})
-end
+
+-- local language_servers = require("lspconfig").util._available_servers() -- or list servers manually like {'gopls', 'clangd'}
+-- for _, ls in ipairs(language_servers) do
+-- 	require("lspconfig")[ls].setup({
+-- 		capabilities = capabilities,
+-- 		-- you can add other fields for setting up lsp server in this table
+-- 	})
+-- end
 
 ufo.setup({
 	provider_selector = function(bufnr, filetype, buftype)
