fd53f40a7ae53e2bde67556751e0aa83362c84b5
jinmay <britzegs@gmail.com>
2025-03-17 10:44:38 +0900

    Update .config/nvim/lua/plugins/fold.lua

--- private_dot_config/nvim/lua/plugins/fold.lua
+++ private_dot_config/nvim/lua/plugins/fold.lua
@@ -5,36 +5,16 @@ vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
+-- folded 배경색 변경
+vim.cmd([[highlight Folded guibg=null]])
+
+-- keymap
 vim.keymap.set("n", "zN", ufo.openAllFolds)
 vim.keymap.set("n", "zM", ufo.closeAllFolds)
 
--- peek folded lines under cursor
-vim.keymap.set("n", "K", function()
-	local winid = require("ufo").peekFoldedLinesUnderCursor()
-	if not winid then
-		vim.lsp.buf.hover()
-	end
-end)
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-	dynamicRegistration = false,
-	lineFoldingOnly = true,
-}
-
--- local language_servers = require("lspconfig").util._available_servers() -- or list servers manually like {'gopls', 'clangd'}
--- for _, ls in ipairs(language_servers) do
--- 	require("lspconfig")[ls].setup({
--- 		capabilities = capabilities,
--- 		-- you can add other fields for setting up lsp server in this table
--- 	})
--- end
-
+-- setup
 ufo.setup({
 	provider_selector = function(bufnr, filetype, buftype)
-		return { "lsp", "indent" }
+		return { "treesitter", "indent" }
 	end,
 })
-
--- folded 배경색 변경
-vim.cmd([[highlight Folded guibg=null]])
