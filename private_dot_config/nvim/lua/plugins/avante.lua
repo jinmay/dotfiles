1658137b1af662ec346825d5ab17471f41b1ac9e
jinmay <britzegs@gmail.com>
2025-06-09 08:46:19 +0900

    Update .config/nvim/lua/plugins/avante.lua

--- private_dot_config/nvim/lua/plugins/avante.lua
+++ private_dot_config/nvim/lua/plugins/avante.lua
@@ -6,21 +6,27 @@ require("avante_lib").load()
 require("avante").setup({
 	provider = "openai",
 	auto_suggestions_provider = "openai",
-	copilot = {
-		endpoint = "https://api.githubcopilot.com",
-		model = "gpt-4o",
-		proxy = nil,
-		allow_insecure = false,
-		timeout = 30000,
-		temperature = 0,
-		max_tokens = 4096,
-	},
-	openai = {
-		endpoint = "https://api.openai.com/v1",
-		model = "gpt-4.1",
-		api_key_name = "OPENAI_AVANTE_API_KEY",
-		timeout = 30000,
-		temperature = 0,
-		max_tokens = 8192,
+	providers = {
+		copilot = {
+			endpoint = "https://api.githubcopilot.com",
+			model = "gpt-4o",
+			proxy = nil,
+			allow_insecure = false,
+			timeout = 30000,
+			extra_request_body = {
+				max_tokens = 4096,
+				temperature = 0,
+			},
+		},
+		openai = {
+			endpoint = "https://api.openai.com/v1",
+			model = "gpt-4.1",
+			api_key_name = "OPENAI_AVANTE_API_KEY",
+			timeout = 30000,
+			extra_request_body = {
+				max_tokens = 8192,
+				temperature = 0,
+			},
+		},
 	},
 })
