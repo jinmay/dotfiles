8366c471e2aca8af5133f308da751fe47a48fb7b
jinmay <britzegs@gmail.com>
2025-09-03 13:34:51 +0900

    Update .config/nvim/lua/plugins/setup.lua

--- private_dot_config/nvim/lua/plugins/setup.lua
+++ private_dot_config/nvim/lua/plugins/setup.lua
@@ -89,7 +89,6 @@ require("lazy").setup({
 		"linux-cultist/venv-selector.nvim",
 		dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
 		lazy = false,
-		branch = "regexp",
 	},
 	{
 		"nvim-neotest/neotest",
